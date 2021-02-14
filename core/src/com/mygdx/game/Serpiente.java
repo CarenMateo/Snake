4503c1a444c437496fc4802ae2b39967530821d7
Ángel Doña García <77341507+Andoga@users.noreply.github.com>
2021-02-14 21:20:17 +0100

    Angel (#5)
    * Estado teclado(Ándres) y parte de Serpiente
    
    * serpiente java

--- core/src/com/mygdx/game/Serpiente.java
+++ core/src/com/mygdx/game/Serpiente.java
@@ -9,51 +9,97 @@ public class Serpiente {
     ///////////
     //Estado//
     //////////
-
-    protected ArrayList<Pieza> cuerpo;
+    protected int posX,posY,ancho;
+    protected ArrayList<Pieza> miCuerpo;
     protected int direccion;
 
     ///////////////////
     ///COMPORTAMIENTO//
     ///////////////////
 
-    public Serpiente(int PosX,int PosY, int nancho){
-        cuerpo= new ArrayList();
-        cuerpo.add(new Pieza(PosX,PosY, nancho));
+    public Serpiente(int posX,int posY, int ancho){
+       Pieza nuevaCabeza;
+       nuevaCabeza = new Pieza(posX,posY,ancho);
+
+       direccion = Pieza.ARR;
+       this.posX = posX;
+       this.posY = posY;
+       this.ancho = ancho;
+
+       miCuerpo = new ArrayList();
+       miCuerpo.add(nuevaCabeza);
+
+
+    }
+    public Serpiente(Serpiente antigua){
+        Pieza nuevaCabeza;
+
+        posX= antigua.getPosX();
+        posY = antigua.getPosY();
+        ancho = antigua.getAncho();
+
+        nuevaCabeza = new Pieza(posX,posY,ancho);
+
+        direccion = Pieza.ARR;
+
+        miCuerpo = new ArrayList();
+        miCuerpo.add(nuevaCabeza);
     }
 
-    //pintate
-    public void render(SpriteBatch miSB){
-        for (int i=0; i< cuerpo.size();i++){
-            cuerpo.get(i).render(miSB);
-        }
-    }
     //moverse
-    public void  moverse(int direccion){
-        switch (direccion){
-            case Pieza.DER:
-                //1.Crear una pieza exactamente en la misma posicion que la cabeza
-                //2.mover esa pieza a la derecha(Solo esto dentro del switch, lo demas fuera)
-                //3. Añadir esa pieza a mi conjunto
-                //4.Eliminar la ultima posicion del array(acordarnos de hacer el dispose)
-                break;
-             case Pieza.IZQ:
-                 break;
-            case Pieza.ARR:
-                break;
-            case Pieza.ABJ:
-                break;
-        }
-
+    public void  moverse(){
+        this.crecer();
     }
+
     public void crecer(){
-        //Lo mismo que moverse, esto es llamado en el controlador cada x frames
+       Pieza nuevaCabeza;
+       Pieza cabezaAntigua = miCuerpo.get(0);
+       nuevaCabeza = new Pieza(cabezaAntigua);
+       nuevaCabeza.moverse(direccion);
+       miCuerpo.add(0,nuevaCabeza);
     }
-    public void dispose(){
-        for(int i=0; i<cuerpo.size();i++){
-            cuerpo.get(i).dispose();
+
+    public void pintarse (SpriteBatch miSB){
+        for(Pieza unaPieza: miCuerpo) {
+            unaPieza.render(miSB);
         }
     }
 
+    public void dispose(){
+        for(Pieza unaPieza: miCuerpo) {
+            unaPieza.dispose();
+        }
+    }
+    public int getPosX(){ return posX;}
+    public int getPosY(){ return posY;}
+    public int getAncho(){ return ancho;}
 
+    //comportamiento hasMuerto
+    public boolean hasMuerto() {
+        boolean resultado;
+        if(testCuerpo()) {
+            resultado = true;
+        } else if (testParedes()) {
+            resultado = true;
+        } else resultado = false;
+
+        return resultado;
+    }
+    private boolean testCuerpo(){
+        // +(0) 1 2 3 4 5 6
+        //tengo que mirar si colisionan una a una las piezas del cuerpo con la cabeza
+        Pieza cabezona = this.miCuerpo.get(0);
+
+        if (miCuerpo.size()<4) return false;
+
+        for(int i=4;i<miCuerpo.size();i++){
+            if (miCuerpo.get(i).colisiona(cabezona)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean testParedes(){
+
+    }
 }
